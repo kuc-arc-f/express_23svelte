9276614b86f04f20269d41608f543462f27644c7
_ñ <knaka0209@gmail.com>
2024-01-20 11:52:45 +0900

    up, 0120a3

--- src/client/Test1.svelte
+++ src/client/Test1.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
+export let items = [];
+
+import HttpCommon from './lib/HttpCommon';
 //console.log("#about");
+
 //
 async function handleClick() {
 console.log('clicked');
-  try{  
-    const url = "https://kuc-hono-47svelte.pages.dev/api/test1"
-		const res = await fetch(url);
-    const json = await res.json()
-console.log(json);
+  try{
+    const postItem = {
+      userId: 0,
+    }
+//console.log(postItem); 
+    const json = await HttpCommon.post(postItem, "/test/get_list");
+    items = json.data;  
+console.log(items);
   } catch (error) {
     console.error(error);
   }   
@@ -18,6 +25,14 @@ console.log(json);
 <div>
   <hr />
   <h1>Test1</h1>
+  <hr />
+  {#each items as item}
+  <div>
+    <h3><a href={`/chats/show/${item.id}`}>{item.title}</a>
+    </h3>
+    <hr />
+  </div>
+  {/each}
   <hr />
   <button on:click={handleClick} class="btn btn-primary">Test</button>
 </div>
