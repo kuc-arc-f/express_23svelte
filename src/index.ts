899501972a1f0426fad054d4916112ef03798cc3
_ñ <knaka0209@gmail.com>
2024-01-26 20:06:02 +0900

    up, 0126a11

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 import { renderToString } from 'react-dom/server';
 //
@@ -9,6 +11,7 @@ import testRouter from './routes/test';
 import commonRouter from './routes/common';
 //
 require('dotenv').config();
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
--- src/routes/test.ts
+++ src/routes/test.ts
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 //import LibTodo from '../lib/LibTodo';
+import LibPg from '../lib/LibPg';
 
 /*****************************
 todos -index
@@ -10,6 +11,31 @@ router.post('/test', async function(req: any, res: any) {
 //    const items = await LibTodo.getItems(req);
 console.log(req.body);
     res.json([]);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+/**
+* 
+* @param
+*
+* @return
+*/ 
+router.post('/test1', async function(req: any, res: any) {
+  try {
+console.log(req.body);
+      //const body = req.body;      
+//console.log(body.userId);
+    const text = `
+      SELECT * FROM public."Todo"
+      ORDER BY id DESC LIMIT 100
+    `;
+    const client = LibPg.getClient();
+    const resulete = await client.query(text);
+    client.end();
+//      console.log(res.rows);
+    res.json({ret: "OK", data: resulete.rows});
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
